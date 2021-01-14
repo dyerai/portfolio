103fc852ae535790b826499b7485b735e13e7cae
dyerai <dyerai734@yahoo.com>
2021-01-14 16:13:30 -0500

    finished navbar functionality

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import { Navbar, Nav, Button } from 'react-bootstrap'
+import { Github, Linkedin } from 'react-bootstrap-icons'
+
+export default function NavBar(props) {
+
+    return (
+        <Navbar bg="dark" variant="dark" fixed="top">
+            <Navbar.Brand href="#home">Aidan Dyer</Navbar.Brand>
+            <Nav className="mr-auto">
+                <Nav.Link href="#about">About</Nav.Link>
+                <Nav.Link href="#skills">Skills</Nav.Link>
+                <Nav.Link href="#projects">Projects</Nav.Link>
+                <Nav.Link href="#contact">Contact</Nav.Link>
+            </Nav>
+           <Nav className="nav justify-content-end">
+                <Button variant="link" href="https://github.com/dyerai" >
+                    <Github size={20} />
+                </Button>
+                <Button variant="link" href="https://www.linkedin.com/in/dyerai/">
+                    <Linkedin size={20} />  
+                </Button>
+           </Nav>
+        </Navbar>
+    );
+}
